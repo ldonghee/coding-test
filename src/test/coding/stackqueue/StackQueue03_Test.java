c167234d90ba6eb353a29414937a647ce85490fc
Donghui-Lee <ehdgml3206@naver.com>
2021-10-18 14:01:44 +0900

    test: 스택/큐 - 다리를 지나는 트럭 테스트 코드 추가

---
+++ src/test/coding/stackqueue/StackQueue03_Test.java
@@ -0,0 +1,50 @@
+package test.coding.stackqueue;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dhlee.coding.stackqueue.StackQueue03;
+
+@DisplayName("Stack Queue")
+public class StackQueue03_Test {
+
+    final StackQueue03 test = new StackQueue03();
+
+    @Test
+    void test_01() {
+        int[] truck_weights = {7, 4, 5, 6};
+        int bridge_length = 2;
+        int weight = 10;
+        int answer = 8;
+
+        int result = test.solution(bridge_length, weight, truck_weights);
+
+        assertThat(result).isEqualTo(answer);
+    }
+
+    @Test
+    void test_02() {
+        int[] truck_weights = {10};
+        int bridge_length = 100;
+        int weight = 100;
+        int answer = 101;
+
+        int result = test.solution(bridge_length, weight, truck_weights);
+
+        assertThat(result).isEqualTo(answer);
+    }
+
+    @Test
+    void test_03() {
+        int[] truck_weights = {10,10,10,10,10,10,10,10,10,10};
+        int bridge_length = 100;
+        int weight = 100;
+        int answer = 110;
+
+        int result = test.solution(bridge_length, weight, truck_weights);
+
+        assertThat(result).isEqualTo(answer);
+    }
+}
