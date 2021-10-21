005215de33ae89905ee0551ef3e5b55787c0754a
Donghui-Lee <ehdgml3206@naver.com>
2021-10-21 22:36:56 +0900

    test: 해시 - 위장 테스트 코드 추가

---
+++ src/test/coding/hash/Hash03_Test.java
@@ -0,0 +1,33 @@
+package test.coding.hash;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dhlee.coding.hash.Hash03;
+
+@DisplayName("Hash")
+public class Hash03_Test {
+    final Hash03 test = new Hash03();
+
+    @Test
+    void test_01() {
+        String[][] clothes = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        int answer = 5;
+
+        int result = test.solution(clothes);
+
+        assertThat(result).isEqualTo(answer);
+    }
+
+    @Test
+    void test_02() {
+        String[][] clothes = {{"crowmask", "face"}, {"bluesunglasses", "face"}, {"smoky_makeup", "face"}};
+        int answer = 3;
+
+        int result = test.solution(clothes);
+
+        assertThat(result).isEqualTo(answer);
+    }
+}
