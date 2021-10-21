f707093827b6581cccefede815287622c1507c98
Donghui-Lee <ehdgml3206@naver.com>
2021-10-21 22:25:41 +0900

    feat: 해시 - 전화번호 목록 구현

--- src/dhlee/coding/hash/Hash02.java
+++ src/dhlee/coding/hash/Hash02.java
@@ -1,5 +1,8 @@
 package dhlee.coding.hash;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 해시 - 전화번호 목록
  *
@@ -37,7 +40,19 @@ package dhlee.coding.hash;
  */
 public class Hash02 {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        return answer;
+        Map<String, String> map = new HashMap<>();
+
+        for (String s : phone_book) {
+            map.put(s, s);
+        }
+
+        for (int i=0; i<phone_book.length; i++) {
+            for (int j=1; j<phone_book[i].length(); j++) {
+                if (map.containsKey(phone_book[i].substring(0, j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
