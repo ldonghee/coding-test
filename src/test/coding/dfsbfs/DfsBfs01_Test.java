4a8e204fd25323003fd9057dab38ba8e74fd47e3
Donghui-Lee <ehdgml3206@naver.com>
2021-10-25 23:00:45 +0900

    feat: 깊이/너비 우선 탐색(DFS/BFS) - 타겟 넘버 - 구현

--- src/test/coding/dfsbfs/DfsBfs01_Test.java
+++ src/test/coding/dfsbfs/DfsBfs01_Test.java
@@ -2,6 +2,7 @@ package test.coding.dfsbfs;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import dhlee.coding.dfsbfs.DfsBfs01;
@@ -14,6 +15,30 @@ public class DfsBfs01_Test {
         int[] numbers = {1, 1, 1, 1, 1};
         int target = 3;
         int answer = 5;
+
+        int result = test.solution(numbers, target);
+
+        assertThat(result).isEqualTo(answer);
+    }
+
+    @Test
+    @DisplayName("원소가 하나일 경우, 테스트")
+    void test_02() {
+        int[] numbers = {5};
+        int target = 5;
+        int answer = 1;
+
+        int result = test.solution(numbers, target);
+
+        assertThat(result).isEqualTo(answer);
+    }
+
+    @Test
+    @DisplayName("target 과 일치하지 않을 경우, 테스트")
+    void test_03() {
+        int[] numbers = {1, 1, 1, 1, 1};
+        int target = 100;
+        int answer = 0;
 
         int result = test.solution(numbers, target);
 
