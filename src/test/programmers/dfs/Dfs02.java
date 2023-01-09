cefdcb798436ac7a07b1dcc5c3deb97fe05ebf09
donghui.lee <donghui.lee@bugs.co.kr>
2023-01-09 12:54:01 +0900

    refactor: 프로그래머스 > DFS/BFS > 게임 맵 최단거리 (베스트 정답 추가)

--- src/test/programmers/dfs/Dfs02.java
+++ src/test/programmers/dfs/Dfs02.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,10 +18,6 @@ public class Dfs02 {
         ArrayList<Integer> answers = new ArrayList<>();
 
         public int solution(int[][] maps) {
-            int answer = 0;
-            // maps.length = 세로
-            // maps[0].length = 가로
-
             boolean[][] visited = new boolean[maps.length][maps[0].length];
             visited[0][0] = true;
 
@@ -28,23 +26,24 @@ public class Dfs02 {
             return answers.size() == 0 ? -1 : answers.stream().min(Comparator.comparing(x -> x.intValue())).get() ;
         }
 
+
         public void dfs(int depth, int[][] maps, boolean[][] visited, int[] pos) {
-            if (pos[1] + 1 < maps[0].length && !visited[pos[0]][pos[1] + 1] && maps[pos[0]][pos[1]] == 1) {
+            if (pos[1] + 1 < maps[0].length && !visited[pos[0]][pos[1] + 1] && maps[pos[0]][pos[1] + 1] == 1) {
                 // 0, +1
                 visited[pos[0]][pos[1] + 1] = true;
                 dfs(depth + 1, maps, visited, new int[]{pos[0], pos[1] + 1});
             }
-            if (pos[0] + 1 < maps.length && !visited[pos[0] + 1][pos[1]] && maps[pos[0]][pos[1]] == 1) {
+            if (pos[0] + 1 < maps.length && !visited[pos[0] + 1][pos[1]] && maps[pos[0] + 1][pos[1]] == 1) {
                 // +1, 0
                 visited[pos[0] + 1][pos[1]] = true;
                 dfs(depth + 1, maps, visited, new int[]{pos[0] + 1, pos[1]});
             }
-            if (pos[1] - 1 > 0 && !visited[pos[0]][pos[1] - 1] && maps[pos[0]][pos[1]] == 1) {
+            if (pos[1] - 1 >= 0 && !visited[pos[0]][pos[1] - 1] && maps[pos[0]][pos[1] - 1] == 1) {
                 // 0, -1
                 visited[pos[0]][pos[1] - 1] = true;
                 dfs(depth + 1, maps, visited, new int[]{pos[0], pos[1] - 1});
             }
-            if (pos[0] - 1 > 0 && !visited[pos[0] - 1][pos[1]] && maps[pos[0]][pos[1]] == 1) {
+            if (pos[0] - 1 >= 0 && !visited[pos[0] - 1][pos[1]] && maps[pos[0] - 1][pos[1]] == 1) {
                 // -1, 0
                 visited[pos[0] - 1][pos[1]] = true;
                 dfs(depth + 1, maps, visited, new int[]{pos[0] - 1, pos[1]});
@@ -56,6 +55,46 @@ public class Dfs02 {
                 return;
             }
         }
+
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {-1, 1, 0, 0};
+
+        public int solution2(int[][] maps) {
+            int answer = 0;
+            int[][] visited = new int[maps.length][maps[0].length];
+            visited[0][0] = 1;
+
+            dfsBest(maps, visited);
+
+            answer = visited[maps.length - 1][maps[0].length - 1];
+
+            if (answer == 0) {
+                return -1;
+            }
+
+            return answer;
+        }
+
+        public void dfsBest(int[][] maps, int[][] visited) {
+            Queue<int[]> q = new LinkedList<>();
+            q.add(new int[]{0, 0});
+
+            while(!q.isEmpty()) {
+                int[] now = q.poll();
+                int x = now[0];
+                int y = now[1];
+
+                for (int i=0; i<4; i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+
+                    if (nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length && visited[nx][ny] == 0 && maps[nx][ny] == 1) {
+                        visited[nx][ny] = visited[x][y] + 1;
+                        q.add(new int[]{nx, ny});
+                    }
+                }
+            }
+        }
     }
 
     private final Solution solution = new Solution();
@@ -65,7 +104,7 @@ public class Dfs02 {
         int[][] maps = {{1,0,1,1,1}, {1,0,1,0,1}, {1,0,1,1,1}, {1,1,1,0,1}, {0,0,0,0,1}};
 
         int expect = 11;
-        int actual = solution.solution(maps);
+        int actual = solution.solution2(maps);
         assertThat(actual).isEqualTo(expect);
     }
 
@@ -74,7 +113,7 @@ public class Dfs02 {
         int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,0},{0,0,0,0,1}};
 
         int expect = -1;
-        int actual = solution.solution(maps);
+        int actual = solution.solution2(maps);
         assertThat(actual).isEqualTo(expect);
     }
 
@@ -88,7 +127,7 @@ public class Dfs02 {
         int[][] maps = {{1, 0, 0},{1, 1, 0},{0, 1, 1}};
 
         int expect = 5;
-        int actual = solution.solution(maps);
+        int actual = solution.solution2(maps);
         assertThat(actual).isEqualTo(expect);
     }
 
@@ -100,7 +139,7 @@ public class Dfs02 {
         int[][] maps = {{1}};
 
         int expect = 1;
-        int actual = solution.solution(maps);
+        int actual = solution.solution2(maps);
         assertThat(actual).isEqualTo(expect);
     }
 
@@ -113,7 +152,7 @@ public class Dfs02 {
         int[][] maps = {{1, 1, 1, 1, 1}, {0, 0 ,0 ,0, 1}};
 
         int expect = 6;
-        int actual = solution.solution(maps);
+        int actual = solution.solution2(maps);
         assertThat(actual).isEqualTo(expect);
     }
 
@@ -126,7 +165,7 @@ public class Dfs02 {
         int[][] maps = {{1, 0, 1, 1, 1}, {0, 0 ,0 ,0, 1}};
 
         int expect = -1;
-        int actual = solution.solution(maps);
+        int actual = solution.solution2(maps);
         assertThat(actual).isEqualTo(expect);
     }
 }
