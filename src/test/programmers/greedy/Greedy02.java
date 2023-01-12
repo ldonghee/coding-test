0931d53900c2bfac12ae886fc515e0d7cdfc34df
donghui.lee <donghui.lee@bugs.co.kr>
2023-01-12 09:27:30 +0900

    refactor: 프로그래머스 > Greedy > 조이스틱 - 수정중

--- src/test/programmers/greedy/Greedy02.java
+++ src/test/programmers/greedy/Greedy02.java
@@ -2,6 +2,10 @@ package test.programmers.greedy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -35,6 +39,55 @@ public class Greedy02 {
 
             return answer + LR;
         }
+
+        List<Integer> answers = new ArrayList<>();
+
+        public int solution2(String name) {
+            int answer = 0;
+
+            boolean[] visited = new boolean[name.length()];
+            dfs(0, 0, 0, name, visited, false);
+
+            answer = answers.stream().min(Comparator.comparing(x -> x)).get();
+            return answer - 1;
+        }
+
+        public void dfs(int depth, int index, int count, String name, boolean[] visited, boolean isRight) {
+            // 왼쪽 오른쪽
+            if (isAllVisited(visited)) {
+                answers.add(count);
+            }
+
+            if (index < 0 || index > name.length()) {
+                return;
+            }
+
+            visited[index] = true;
+
+            if (name.charAt(index) != 'A') {
+                // move
+                count += 1;
+                // up/down
+                count += Math.min(name.charAt(index) - 65, 26 - (name.charAt(index) - 65));
+            }
+
+
+            // 오른쪽 방향
+            if (isRight) {
+                dfs(depth + 1, index + 1, count, name, visited, true);
+            }
+            // 왼쪽 방향
+            if (!isRight){
+                dfs(depth + 1, name.length() - (depth + 1), count, name, visited, false);
+            }
+        }
+
+        public boolean isAllVisited(boolean[] visited) {
+            int count = 0;
+            for (boolean v : visited)
+                count += v ? 1 : 0;
+            return count == visited.length;
+        }
     }
 
     private final Solution solution = new Solution();
@@ -44,7 +97,7 @@ public class Greedy02 {
         String name = "JEROEN";
 
         int expect = 56;
-        int actual = solution.solution(name);
+        int actual = solution.solution2(name);
         assertThat(actual).isEqualTo(expect);
     }
 
@@ -53,7 +106,7 @@ public class Greedy02 {
         String name = "JAN";
 
         int expect = 23;
-        int actual = solution.solution(name);
+        int actual = solution.solution2(name);
         assertThat(actual).isEqualTo(expect);
     }
 }
