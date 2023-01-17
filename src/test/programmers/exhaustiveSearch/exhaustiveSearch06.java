e1d95d5651af89e199976c9f374ebbb2888959f2
donghui.lee <donghui.lee@bugs.co.kr>
2023-01-18 08:36:54 +0900

    refactor: 프로그래머스 > 완전탐색 > 전력망을 둘로 나누기 - 정답 copy

--- src/test/programmers/exhaustiveSearch/exhaustiveSearch06.java
+++ src/test/programmers/exhaustiveSearch/exhaustiveSearch06.java
@@ -2,6 +2,11 @@ package test.programmers.exhaustiveSearch;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -10,15 +15,53 @@ import org.junit.jupiter.api.Test;
  */
 public class exhaustiveSearch06 {
 	public class Solution {
+		public List<Integer>[] list;
+
 		public int solution(int n, int[][] wires) {
-			int answer = -1;
+			int answer = 100;
+
+			list = new List[n + 1];
+			for (int i = 0; i <= n; i++) {
+				list[i] = new ArrayList<>();
+			}
+
+			for (int[] wire : wires) {
+				list[wire[0]].add(wire[1]);
+				list[wire[1]].add(wire[0]);
+			}
+
+			for (int[] wire : wires) {
+				int n1 = bfs(wire[0], wire[1], n);
+				int n2 = bfs(wire[1], wire[0], n);
+
+				answer = Math.min(answer, Math.abs(n1 - n2));
+			}
+
 			return answer;
 		}
 
-		public void dfs(int depth, int[][] wires) {
-			for (int i = depth + 1; i>wires.length; i++) {
+		public int bfs(int v1, int v2, int n) {
+			Queue<Integer> queue = new LinkedList<>();
+			boolean[] visit = new boolean[n + 1];
 
+			int cnt = 0;
+
+			queue.add(v1);
+			visit[v1] = true;
+
+			while (!queue.isEmpty()) {
+				int cur = queue.poll();
+				cnt++;
+
+				for (int next : list[cur]) {
+					if (next != v2 && !visit[next]) {
+						queue.add(next);
+						visit[next] = true;
+					}
+				}
 			}
+
+			return cnt;
 		}
 	}
 
