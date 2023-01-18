125b7f405bec06278518d6d8e81321217df346bf
donghui.lee <donghui.lee@bugs.co.kr>
2023-01-19 08:47:47 +0900

    feat: 프로그래머스 > DP > 정수 삼각형 - 정답 copy

--- src/test/programmers/dynamicProgramming/Dp02.java
+++ src/test/programmers/dynamicProgramming/Dp02.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
  */
 public class Dp02 {
 
-	class Solution {
+	class Solution2 {
 
 		int max = 0;
 		public int solution(int[][] triangle) {
@@ -38,27 +38,43 @@ public class Dp02 {
 		}
 	}
 
-	/**
-	 *    7
-	 *    3, 8
-	 *    8, 1, 8
-	 *    2, 7, 4, 4
-	 *    4, 5, 2, 6, 5
-	 *
-	 *          7
-	 *        3   8
-	 *      8   1   8
-	 *    2   7   4   4
-	 *  4   5   2   6   5
-	 *
-	 *          0
-	 *        0   1
-	 *      0   1   2
-	 *    0   1   2   3
-	 *  0   1   2   3   4
-	 *
-	 *  curIndex + 0, 1
-	 */
+	class Solution {
+
+
+		/**
+		 *    7
+		 *    3, 8
+		 *    8, 1, 8
+		 *    2, 7, 4, 4
+		 *    4, 5, 2, 6, 5
+		 *
+		 *          7
+		 *        3   8
+		 *      8   1   8
+		 *    2   7   4   4
+		 *  4   5   2   6   5
+		 */
+
+		public int solution(int[][] triangle) {
+			int answer = 0;
+
+			int[][] temp = new int[triangle.length][triangle[triangle.length - 1].length];
+			temp[0][0] = triangle[0][0];
+
+			for (int i=1; i<triangle.length; i++) {
+				temp[i][0] = temp[i - 1][0] + triangle[i][0];
+				temp[i][i] = temp[i - 1][i - 1] + triangle[i][i];
+
+				for (int j=1; j<=i-1; j++) {
+					temp[i][j] = Math.max(temp[i - 1][j - 1], temp[i - 1][j]) + triangle[i][j];
+
+					answer = Math.max(answer, temp[i][j]);
+				}
+ 			}
+
+			return answer;
+		}
+	}
 
 	private final Solution solution = new Solution();
 
