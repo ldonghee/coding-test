8fad8d6d80586385d6585bdf85c27f9edc4e4094
Donghui-Lee <ehdgml3206@naver.com>
2023-01-30 23:09:23 +0900

    refactor: 프로그래머스 > DP > 등굣길 (효율성 실패)

--- src/test/programmers/dynamicProgramming/Dp03.java
+++ src/test/programmers/dynamicProgramming/Dp03.java
@@ -13,64 +13,81 @@ import org.junit.jupiter.api.Test;
  */
 public class Dp03 {
 
-	class Solution {
+    class Solution {
 
-		boolean[][] visited;
-		List<Integer> answers = new ArrayList<>();
-		int shortestPath = 1000000007;
+        List<Integer> answers = new ArrayList<>();
+        int shortestPath = 1000000007;
 
-		public int solution(int m, int n, int[][] puddles) {
-			visited = new boolean[n][m];
-			List<int[]> puddleList = new ArrayList<>();
-			for (int i=0; i<puddles.length; i++) {
-				puddleList.add(puddles[i]);
-			}
+        public int solution(int m, int n, int[][] puddles) {
+            List<int[]> puddleList = new ArrayList<>();
+            for (int i=0; i<puddles.length; i++) {
+                puddleList.add(puddles[i]);
+            }
 
-			dfs(0, m, n, puddleList, 0, 0);
+            dfs(0, m, n, puddleList, 0, 0);
 
-			int answer = (int) answers.stream().filter(a -> a == shortestPath).count();
+            int answer = (int) answers.stream().filter(a -> a == shortestPath).count();
 
-			return answer % 1000000007;
-		}
+            return answer % 1000000007;
+        }
 
-		public void dfs(int depth, int m, int n, List<int[]> puddleList, int posX, int posY) {
-			if (posX == m - 1 && posY == n - 1) {
-				answers.add(depth);
-				if (depth < shortestPath) {
-					shortestPath = depth;
-				}
-				return;
-			}
-			for (int i=0; i<puddleList.size(); i++) {
-				int[] puddle = puddleList.get(i);
-				if (puddle[0] == posY + 1 && puddle[1] == posX + 1) {
-					return;
-				}
-			}
+        public void dfs(int depth, int m, int n, List<int[]> puddleList, int posX, int posY) {
+            if (posX == m - 1 && posY == n - 1) {
+                answers.add(depth);
+                if (depth < shortestPath) {
+                    shortestPath = depth;
+                }
+                return;
+            }
+            for (int i=0; i<puddleList.size(); i++) {
+                int[] puddle = puddleList.get(i);
+                if (puddle[0] == posX + 1 && puddle[1] == posY + 1) {
+                    return;
+                }
+            }
 
-			if (posX < m && posY < n) {
-				visited[posY][posX] = true;
-				dfs(depth + 1, m, n, puddleList, posX + 1, posY);
-				visited[posY][posX] = false;
-			}
+            if (posX < m && posY < n) {
+                dfs(depth + 1, m, n, puddleList, posX + 1, posY);
+            }
 
-			if (posY < n && posX < m) {
-				visited[posY][posX] = true;
-				dfs(depth + 1, m, n, puddleList, posX, posY + 1);
-				visited[posY][posX] = false;
-			}
-		}
-	}
-	private final Solution solution = new Solution();
+            if (posY < n && posX < m) {
+                dfs(depth + 1, m, n, puddleList, posX, posY + 1);
+            }
+        }
+    }
 
-	@Test
-	public void test01() {
-		int m = 4;
-		int n = 3;
-		int[][] puddles = {{2, 2}};
+    private final Solution solution = new Solution();
 
-		int expect = 4;
-		int actual = solution.solution(m, n, puddles);
-		assertThat(actual).isEqualTo(expect);
-	}
+    @Test
+    public void test01() {
+        int m = 4;
+        int n = 3;
+        int[][] puddles = {{2, 2}};
+
+        int expect = 4;
+        int actual = solution.solution(m, n, puddles);
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    @Test
+    public void test02() {
+        int m = 4;
+        int n = 3;
+        int[][] puddles = {{2, 2}, {3, 2}};
+
+        int expect = 2;
+        int actual = solution.solution(m, n, puddles);
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    @Test
+    public void test03() {
+        int m = 4;
+        int n = 3;
+        int[][] puddles = {{2, 1}, {1, 3}, {2, 3}, {3, 3}};
+
+        int expect = 1;
+        int actual = solution.solution(m, n, puddles);
+        assertThat(actual).isEqualTo(expect);
+    }
 }
