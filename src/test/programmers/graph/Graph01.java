e6c3834b65e3e2a4039186419e0bb1ce716a6f8a
donghui.lee <donghui.lee@bugs.co.kr>
2023-02-01 09:07:59 +0900

    refactor: 프로그래머스 > 그래프 > 가장 먼 노드 (임시)

--- src/test/programmers/graph/Graph01.java
+++ src/test/programmers/graph/Graph01.java
@@ -2,6 +2,11 @@ package test.programmers.graph;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -10,9 +15,52 @@ import org.junit.jupiter.api.Test;
  */
 public class Graph01 {
 	class Solution {
+		public List<Integer>[] list;
+
 		public int solution(int n, int[][] edge) {
 			int answer = 0;
+
+			list = new List[n + 1];
+			for (int i = 1; i <= n; i++) {
+				list[i] = new ArrayList<>();
+			}
+
+			for (int[] wire : edge) {
+				list[wire[0]].add(wire[1]);
+				list[wire[1]].add(wire[0]);
+			}
+
+			for (int i=2; i<=n; i++) {
+				bfs(i);
+			}
+
 			return answer;
+		}
+
+		public int bfs(int target) {
+			Queue<Integer> queue = new LinkedList<>();
+			List<Integer> answers = new ArrayList<>();
+
+			queue.add(1);
+			int cnt = 0;
+
+			while (!queue.isEmpty()) {
+				int cur = queue.poll();
+				cnt++;
+
+				for (int next : list[cur]) {
+					if (next != target) {
+						queue.add(next);
+					}
+
+					if (next == target) {
+						answers.add(cnt);
+						break;
+					}
+				}
+			}
+
+			return cnt;
 		}
 	}
 
