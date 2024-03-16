22c6a635a441cd3a77b4e783e5e5a3df52f660f2
donghui.lee <donghui.lee@bugs.co.kr>
2024-03-16 11:02:49 +0900

    feat: LeeCode 33. Search in Rotated Sorted Array 풀이 코드 추가

--- src/test/neetcode/binarySearch/SearchInRotatedSortedArray.java
+++ src/test/neetcode/binarySearch/SearchInRotatedSortedArray.java
@@ -2,9 +2,6 @@ package test.neetcode.binarySearch;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 /**
@@ -13,7 +10,32 @@ import org.junit.jupiter.api.Test;
  */
 public class SearchInRotatedSortedArray {
 	public int search(int[] nums, int target) {
-		return 0;
+		int left = 0;
+		int right = nums.length - 1;
+
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+
+			if (nums[mid] == target) {
+				return mid;
+			}
+
+			// 왼쪽 정렬이 정상 정렬인 경우 (오름차순)
+			if (nums[left] <= nums[mid]) {
+				if (nums[left] <= target && nums[mid] > target) {
+					right = mid - 1;
+				} else {
+					left = mid + 1;
+				}
+			} else {
+				if (nums[right] < target && nums[mid] <= target) {
+					left = mid + 1;
+				} else {
+					right = mid - 1;
+				}
+			}
+		}
+		return -1;
 	}
 
 	@Test
@@ -45,6 +67,17 @@ public class SearchInRotatedSortedArray {
 		int target = 0;
 
 		int expect = -1;
+		int actual = search(nums, target);
+
+		assertThat(expect).isEqualTo(actual);
+	}
+
+	@Test
+	public void test04() {
+		int[] nums = {7,0,1,2,4,5,6};
+		int target = 0;
+
+		int expect = 1;
 		int actual = search(nums, target);
 
 		assertThat(expect).isEqualTo(actual);
