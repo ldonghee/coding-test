c5739efcede8c184bd8b71f37910c6d98aac4d56
donghui.lee <donghui.lee@bugs.co.kr>
2024-03-31 22:22:28 +0900

    feat: LeeCode 128. Longest Consecutive Sequence 풀이 코드 추가

--- src/test/neetcode/hashTable/LongestConsecutiveSequence.java
+++ src/test/neetcode/hashTable/LongestConsecutiveSequence.java
@@ -2,6 +2,12 @@ package test.neetcode.hashTable;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -10,14 +16,50 @@ import org.junit.jupiter.api.Test;
  */
 public class LongestConsecutiveSequence {
 	public int longestConsecutive(int[] nums) {
-		return 0;
+		Arrays.sort(nums);
+
+		int result = 0;
+		Map<Integer, Integer> countMap = new HashMap<>();
+		for (int num : nums) {
+			int count = countMap.getOrDefault(num - 1, 0) + 1;
+			countMap.put(num, count);
+			if (count > result) {
+				result = count;
+			}
+		}
+
+		return result;
+	}
+
+
+	public int bestLongestConsecutive(int[] nums) {
+		int maxSeq = 0;
+		Set<Integer> sequences = new HashSet();
+		for (int num : nums) {
+			sequences.add(num);
+		}
+		for (int num : nums) {
+			int nextInSeq = num + 1;
+			int prevInSeq = num - 1;
+			int currentSequence = 1;
+			while (sequences.remove(prevInSeq--)) {
+				currentSequence++;
+			}
+			while (sequences.remove(nextInSeq++)) {
+				currentSequence++;
+			}
+			if (currentSequence > maxSeq) {
+				maxSeq = currentSequence;
+			}
+		}
+		return maxSeq;
 	}
 
 	@Test
 	public void test() {
 		int[] nums = {100, 4, 200, 1, 3, 2};
 		int expect = 4;
-		int actual = longestConsecutive(nums);
+		int actual = bestLongestConsecutive(nums);
 		assertThat(actual).isEqualTo(expect);
 	}
 
@@ -25,7 +67,7 @@ public class LongestConsecutiveSequence {
 	public void test2() {
 		int[] nums = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
 		int expect = 9;
-		int actual = longestConsecutive(nums);
+		int actual = bestLongestConsecutive(nums);
 		assertThat(actual).isEqualTo(expect);
 	}
 }
