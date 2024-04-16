ee6586512f5fe613187ef189ce3b5ff678616d3d
donghui.lee <donghui.lee@bugs.co.kr>
2024-04-16 21:02:29 +0900

    refactor: LeeCode 91. Decode Ways 코드 임시 커밋

--- src/test/neetcode/dynamicProgramming/DecodeWays.java
+++ src/test/neetcode/dynamicProgramming/DecodeWays.java
@@ -2,6 +2,9 @@ package test.neetcode.dynamicProgramming;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -9,8 +12,50 @@ import org.junit.jupiter.api.Test;
  * https://leetcode.com/problems/decode-ways/
  */
 public class DecodeWays {
+	/**
+	 * 1 -> A
+	 * 2 -> B
+	 * 3 -> C
+	 * ..
+	 * ..
+	 * 26 -> Z
+	 */
+	List<String> answers = new ArrayList<>();
+
 	public int numDecodings(String s) {
+		// 1 <= s.length <= 100
+
+		// 숫자를 문자로 표현 방식?
+		// 아스키코드 A => 65, Z => 90
+		// A : 65 - 65 + 1 => 1
+		// Z : 90 - 65 + 1 => 26
+
+		// 문자열 s를 어떻게 나눌것인가?
+		//
+		// 2 2 6
+		// 방문 관리 리스트 visited[s.length]
+		//
+
+		dfs(s, 0, s.charAt(0), new StringBuilder());
+
 		return 0;
+	}
+
+	void dfs(String s, int index, char prev, StringBuilder builder) {
+		if (index == s.length()) {
+			String.valueOf((char)(s.charAt(0) + 17))
+
+			answers.add(builder.toString());
+			return;
+		}
+
+		if ((prev + ((int) s.charAt(index)) <= 26) {
+			int ch = ((int) prev) + ((int) s.charAt(index)) + 65;
+			String tempResult = String.valueOf(ch);
+			builder.append(tempResult);
+		}
+
+		dfs(s, index+1, s.charAt(index), builder);
 	}
 
 	@Test
