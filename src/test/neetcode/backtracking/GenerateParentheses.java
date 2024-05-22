2a5d544a83f3b9789bd08534acbdc221a64c597e
ldonghee <ehdgml3206@gamil.com>
2024-05-22 22:54:02 +0900

    feat: LeetCode 22. Generate Parentheses 풀이 코드 추가 (복습)

--- src/test/neetcode/backtracking/GenerateParentheses.java
+++ src/test/neetcode/backtracking/GenerateParentheses.java
@@ -2,8 +2,11 @@ package test.neetcode.backtracking;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,8 +15,33 @@ import org.junit.jupiter.api.Test;
  * https://leetcode.com/problems/generate-parentheses/description/
  */
 public class GenerateParentheses {
+	List<String> answers = new ArrayList<>();
+
 	public List<String> generateParenthesis(int n) {
-		return null;
+		dfs(0, 0, n, new LinkedList<>());
+		return answers;
+	}
+
+	void dfs(int left, int right, int n, List<String> stack) {
+		if (left == n && right == n) {
+			String result = "";
+			for (String s : stack) {
+				result += s;
+			}
+			answers.add(result);
+		}
+
+		if (left < n) {
+			stack.add("(");
+			dfs(left+1, right, n, stack);
+			stack.remove(stack.size() - 1);
+		}
+
+		if (right < left) {
+			stack.add(")");
+			dfs(left, right+1, n, stack);
+			stack.remove(stack.size() - 1);
+		}
 	}
 
 	@Test
