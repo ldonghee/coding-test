b85fcf75c922e48a2c2fd716be43c112a77dfebb
ldonghee <ehdgml3206@gamil.com>
2024-06-13 08:10:54 +0900

    feat: LeeCode 5. Longest Palindromic Substring 풀이 코드 추가

--- src/test/neetcode/twopointers/LongestPalindromicSubstring.java
+++ src/test/neetcode/twopointers/LongestPalindromicSubstring.java
@@ -10,7 +10,47 @@ import org.junit.jupiter.api.Test;
  */
 public class LongestPalindromicSubstring {
 	public String longestPalindrome(String s) {
-		return "";
+		if (s.length() <= 1) {
+			return s;
+		}
+
+		String answer = "";
+		int left = 0;
+		int right = s.length() - 1;
+
+		while (left <= right) {
+			String subStr = s.substring(left, right + 1);
+
+			if (answer.length() >= subStr.length()) {
+				left++;
+				right = s.length() - 1;
+				continue;
+			}
+
+			if (isPalindrome(subStr)) {
+				answer = subStr;
+				left++;
+				right = s.length() - 1;
+			} else {
+				right--;
+			}
+		}
+
+		return answer;
+	}
+
+	public boolean isPalindrome(String s) {
+		int left = 0;
+		int right = s.length() - 1;
+
+		while (left < right) {
+			if (s.charAt(left) != s.charAt(right)) {
+				return false;
+			}
+			left++;
+			right--;
+		}
+		return true;
 	}
 
 	@Test
