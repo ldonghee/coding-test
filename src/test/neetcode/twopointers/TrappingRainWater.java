2d48a6d460ef16e7b351213805f482717fc17bbb
ldonghee <ehdgml3206@gamil.com>
2024-06-18 08:55:10 +0900

    feat: LeeCode 494. Target Sum 풀이 코드 추가

--- src/test/neetcode/twopointers/TrappingRainWater.java
+++ src/test/neetcode/twopointers/TrappingRainWater.java
@@ -12,33 +12,45 @@ import org.junit.jupiter.api.Test;
  */
 public class TrappingRainWater {
 	public int trap(int[] height) {
-		int answer = 0;
+		if (height.length == 0) {
+			return 0;
+		}
 
-		int max = Arrays.stream(height)
-						.max()
-						.getAsInt();
-		for (int i=0; i<height.length-1; i++) {
-			int first = i;
-
-			if (height[first] == max) {
-				continue;
-			}
-
-			for (int next=i+1; next<height.length; next++) {
-				if (height[next] >= height[first]) {
-					i = next - 1;
-					break;
-				}
-				answer += height[first] - height[next];
+		int trap = 0;
+		int top = height[0];
+		int index = 0;
+		// Max Index 구하기
+		for (int i = 0; i < height.length; i++) {
+			if (top <= height[i]) {
+				index = i;
+				top = height[i];
 			}
 		}
 
-		return answer;
+		int left = height[0];
+		for (int i = 0; i < index; i++) {
+			if (height[i] > left) {
+				left = height[i];
+			} else if (height[i] < left) {
+				trap += (left - height[i]);
+			}
+		}
+
+		int right = height[height.length - 1];
+		for (int i = height.length - 1; i > index; i--) {
+			if (height[i] > right) {
+				right = height[i];
+			} else if (height[i] < right) {
+				trap += (right - height[i]);
+			}
+		}
+
+		return trap;
 	}
 
 	@Test
 	public void test01() {
-		int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+		int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
 		int expect = 6;
 		int actual = trap(height);
 		assertThat(expect).isEqualTo(actual);
@@ -46,8 +58,16 @@ public class TrappingRainWater {
 
 	@Test
 	public void test02() {
-		int[] height = {4,2,0,3,2,5};
+		int[] height = {4, 2, 0, 3, 2, 5};
 		int expect = 9;
+		int actual = trap(height);
+		assertThat(expect).isEqualTo(actual);
+	}
+
+	@Test
+	public void test03() {
+		int[] height = {1,0,4,3,3,4,0,1};
+		int expect = 4;
 		int actual = trap(height);
 		assertThat(expect).isEqualTo(actual);
 	}
