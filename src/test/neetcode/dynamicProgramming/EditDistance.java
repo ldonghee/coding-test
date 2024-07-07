6246e1b72e2e54458a85e23b9dd8281e58e1d64e
donghui.lee <donghui.lee@bugs.co.kr>
2024-07-07 14:24:54 +0900

    feat: LeeCode 72. Edit Distance 풀이 코드 추가(이해가 안감)

--- src/test/neetcode/dynamicProgramming/EditDistance.java
+++ src/test/neetcode/dynamicProgramming/EditDistance.java
@@ -9,8 +9,35 @@ import org.junit.jupiter.api.Test;
  * https://leetcode.com/problems/edit-distance/
  */
 public class EditDistance {
+
 	public int minDistance(String word1, String word2) {
-		return 0;
+		int n = word1.length();
+		int m = word2.length();
+		// 다이나믹 프로그래밍을 위한 2차원 DP 테이블 초기화
+		int[][] dp = new int[n + 1][m + 1];
+		// DP 테이블 초기 설정
+		for (int i = 1; i <= n; i++) {
+			dp[i][0] = i;
+		}
+		for (int j = 1; j <= m; j++) {
+			dp[0][j] = j;
+		}
+
+		// 최소 편집 거리 계산
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				// 문자가 같다면, 왼쪽 위에 해당하는 수를 그대로 대입
+				if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+					dp[i][j] = dp[i - 1][j - 1];
+				}
+				// 문자가 다르다면, 세 가지 경우 중에서 최솟값 찾기
+				else { // 삽입(왼쪽), 삭제(위쪽), 교체(왼쪽 위) 중에서 최소 비용을 찾아 대입
+					dp[i][j] = 1 + Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1]));
+				}
+			}
+		}
+
+		return dp[n][m];
 	}
 
 	@Test
@@ -28,4 +55,5 @@ public class EditDistance {
 		int actual = minDistance(word1, word2);
 		assertThat(expect).isEqualTo(actual);
 	}
+
 }
